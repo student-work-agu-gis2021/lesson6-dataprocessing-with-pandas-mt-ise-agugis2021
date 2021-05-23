edaba22c2131aed3120b7b1daf230dc6d0546c43
mtiseagugis2021 <>
2021-05-23 06:58:32 +0000

    I added code check the length of'data'

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -58,7 +58,7 @@ print('Number of no-data values in column "TMIN":', tmin_nodata_count)
 
 day_count = None 
 #YOUR CODE HERE 4
-
+day_count=len(data['DATA'])
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print("Number of days:", day_count)
